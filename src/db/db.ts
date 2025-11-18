8d3c90deb7b1ab4a2bc956a1481e4920bbd78c80
Ulrik-Matemu <advancechronicles@gmail.com>
2025-11-18 20:06:50 +0300

    added postgres db connection

---
+++ src/db/db.ts
@@ -0,0 +1,16 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+const { Pool } = pkg;
+dotenv.config();
+
+
+const pool = new Pool({
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT),
+});
+
+export default pool;
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express from 'express';
-
 const app = express();
 const port = 3000;
+import pool from './db/db';
+
+
+pool.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database', err);
+  });
+
+  
 
 app.get('/', (req, res) => {
   res.send('CCP Backend with TypeScript and Express!');
